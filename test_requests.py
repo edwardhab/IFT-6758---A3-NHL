d16282dabb454606f1cd0d0275ec667683516bc0
Michel Essono <essonomw@gmail.com>
2024-10-08 05:25:04 -0400

    Supprimer certains fichiers indésirables

--- test_requests.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-
-r = requests.get('https://api-web.nhle.com/v1/gamecenter/2022030411/play-by-play',params = {'details': 'game_detail'})
-print(r.json().get('plays').get('typeCode'))
